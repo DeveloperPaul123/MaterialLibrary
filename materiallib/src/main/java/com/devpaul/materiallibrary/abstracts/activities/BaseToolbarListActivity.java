ed22e3ece3a34b232c4d717431bebe5bbaf0a87f
Paul Tsouchlos <ptsouchlos@bellsouth.net>
2018-03-21 21:40:38 -0400

    Use Inheritance Don't Rewrite Code
    Change inheritance scheme so code can be re-used more as the activities do stack on top of each other.

--- materiallib/src/main/java/com/devpaul/materiallibrary/abstracts/activities/BaseToolbarListActivity.java
+++ materiallib/src/main/java/com/devpaul/materiallibrary/abstracts/activities/BaseToolbarListActivity.java
@@ -1,7 +1,6 @@
 package com.devpaul.materiallibrary.abstracts.activities;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
@@ -14,7 +13,7 @@ import com.devpaul.materiallibrary.R;
  *
  * Abstract activity that contains a toolbar and a list view.
  */
-public abstract class BaseToolbarListActivity extends AppCompatActivity {
+public abstract class BaseToolbarListActivity extends BaseToolbarActivity {
 
     /**
      * List view for this activity.
@@ -26,38 +25,15 @@ public abstract class BaseToolbarListActivity extends AppCompatActivity {
      */
     private Toolbar toolbar;
 
-    /**
-     * Content container.
-     */
-    private FrameLayout homeLayout;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.material_library_base_toolbar_activity);
-        toolbar = (Toolbar) findViewById(R.id.material_library_base_toolbar);
-        setSupportActionBar(toolbar);
-        homeLayout = (FrameLayout) findViewById(R.id.material_library_base_activity_content);
-        listView = new ListView(this);
-        listView.setId(android.R.id.list);
-        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        listView.setLayoutParams(params);
-        homeLayout.addView(listView);
-        init();
+        listView = (ListView) findViewInContentById(R.id.material_library_base_list_view);
     }
 
-
-    /**
-     * Perform you initialization here. This is called after on create.
-     */
-    public abstract void init();
-
-    /**
-     * Gets the home layout of this activity.
-     * @return The home FrameLayout of the content.
-     */
-    public FrameLayout getHomeLayout() {
-        return this.homeLayout;
+    @Override
+    public int getLayoutContentId() {
+        return R.layout.material_library_list_view;
     }
 
     /**
